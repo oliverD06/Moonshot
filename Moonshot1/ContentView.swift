cb76a5a65ced709a070d74235710c258342583df
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-10-23 09:55:09 -0500

    Initial Commit

---
+++ Moonshot1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Moonshot1
+//
+//  Created by Oliver Delaney on 10/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot1/Moonshot1App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot1App.swift
+//  Moonshot1
+//
+//  Created by Oliver Delaney on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
